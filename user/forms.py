17610a93f782a4dcc622d734e74c821c67b72578
Anton Shpakovych <shpakovych18@gmail.com>
2024-02-21 11:59:08 +0200

    Added html/css/js for login and signup

--- user/forms.py
+++ user/forms.py
@@ -1,8 +1,20 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields[
+            "email"
+        ].widget.attrs["placeholder"] = "Enter your email"
+        self.fields[
+            "password1"
+        ].widget.attrs["placeholder"] = "Create your password"
+        self.fields[
+            "password2"
+        ].widget.attrs["placeholder"] = "Confirm your password"
+
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ('email', 'password1', 'password2')
@@ -13,3 +25,14 @@ class CustomUserCreationForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    def __init__(self, request=None, *args, **kwargs):
+        super().__init__(request, *args, **kwargs)
+        self.fields[
+            "username"
+        ].widget.attrs["placeholder"] = "Enter email"
+        self.fields[
+            "password"
+        ].widget.attrs["placeholder"] = "Enter password"
--- user/views.py
+++ user/views.py
@@ -1,17 +1,17 @@
 from django.contrib.auth import login
-from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from django.views import View
 
 from config.settings import LOGIN_REDIRECT_URL
-from user.forms import CustomUserCreationForm
+from user.forms import CustomUserCreationForm, CustomAuthenticationForm
 
 
 class LoginOrSignUp(View):
     login_object_name = "form_login"
     signup_object_name = "form_signup"
+    form_to_render_name = "form_to_render"
     template_name = "registration/login.html"
-    login_form_class = AuthenticationForm
+    login_form_class = CustomAuthenticationForm
     signup_form_class = CustomUserCreationForm
 
     def get(self, request):
@@ -45,11 +45,13 @@ class LoginOrSignUp(View):
         if self.is_form_login(request=request):
             return {
                 self.login_object_name: form,
-                self.signup_object_name: self.signup_form_class()
+                self.signup_object_name: self.signup_form_class(),
+                self.form_to_render_name: self.login_object_name
             }
         return {
             self.login_object_name: self.login_form_class(),
-            self.signup_object_name: form
+            self.signup_object_name: form,
+            self.form_to_render_name: self.signup_object_name
         }
 
     def _form_valid(self, request, form):
